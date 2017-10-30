f99a1daace7f5969d5f26286e92ace0c86e46cb3
abrasha <aabrasha@gmail.com>
2017-10-30 17:56:57 +0200

    - add hashtable

--- src/main/java/com/github/abrasha/HashMapVSLinkedHashMap.java
+++ src/main/java/com/github/abrasha/HashMapVSLinkedHashMap.java
@@ -9,8 +9,9 @@ import java.util.concurrent.TimeUnit;
 // Run complete.
 //
 // Benchmark                             Mode  Cnt    Score    Error  Units
-// HashMapVSLinkedHashMap.hashMap        avgt    9  104.324 ± 10.539  ns/op
-// HashMapVSLinkedHashMap.linkedHashMap  avgt    9  170.261 ± 45.108  ns/op
+// HashMapVSLinkedHashMap.hashMap        avgt    9  101.183 ±  7.116  ns/op
+// HashMapVSLinkedHashMap.hashtable      avgt    9  242.710 ± 88.953  ns/op
+// HashMapVSLinkedHashMap.linkedHashMap  avgt    9  150.633 ± 15.713  ns/op
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -40,6 +41,26 @@ public class HashMapVSLinkedHashMap {
     public void linkedHashMap(Blackhole bh) {
     
         Map<Integer, String> map = new LinkedHashMap<>();
+    
+        map.put(0, "zero");
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.put(6, "six");
+        map.put(7, "seven");
+        map.put(8, "eight");
+        map.put(9, "nine");
+    
+        bh.consume(map);
+        
+    }
+    
+    @Benchmark
+    public void hashtable(Blackhole bh) {
+    
+        Map<Integer, String> map = new Hashtable<>();
     
         map.put(0, "zero");
         map.put(1, "one");
