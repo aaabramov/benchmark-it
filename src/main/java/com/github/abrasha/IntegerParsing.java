d8efc0a54a062a77124739cd6ec7770e4109a92e
Andrii <aabrasha@gmail.com>
2017-11-05 12:37:00 +0200

    - add integer parsing benchmark (#7)
    - add integer manual unboxing benchmark
    - update readme
    - add `run-results` to .gitignore

---
+++ src/main/java/com/github/abrasha/IntegerParsing.java
@@ -0,0 +1,34 @@
+package com.github.abrasha;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+// Run complete
+//
+// Benchmark                      Mode   Cnt    Score   Error  Units
+// IntegerParsing.integerParse    avgt    9    23.314 ± 1.402  ns/op
+// IntegerParsing.integerValueOf  avgt    9    25.892 ± 0.758  ns/op
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class IntegerParsing {
+    
+    @State(Scope.Benchmark)
+    public static class NumberHolder {
+        
+        private String num = "95857331";
+        
+    }
+    
+    @Benchmark
+    public void integerParse(NumberHolder state, Blackhole bh) {
+        bh.consume(Integer.parseInt(state.num));
+    }
+    
+    @Benchmark
+    public void integerValueOf(NumberHolder state, Blackhole bh) {
+        bh.consume(Integer.valueOf(state.num));
+    }
+    
+}
