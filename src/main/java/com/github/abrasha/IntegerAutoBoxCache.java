37eb74d82e9427d553ca972710efb7ffe7e1a0ba
Andrii <aabrasha@gmail.com>
2017-11-05 22:32:16 +0200

    - `add `Integer.valueOf(int)` for cached `Integer`s` (#8)

---
+++ src/main/java/com/github/abrasha/IntegerAutoBoxCache.java
@@ -0,0 +1,27 @@
+package com.github.abrasha;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+// Run complete
+//
+// Benchmark                         Mode  Cnt  Score   Error  Units
+// IntegerAutoBoxCache.getCached     avgt    9  2.869 ± 0.094  ns/op
+// IntegerAutoBoxCache.getNonCached  avgt    9  3.997 ± 0.107  ns/op
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class IntegerAutoBoxCache {
+    
+    @Benchmark
+    public void getCached(Blackhole bh) {
+        bh.consume(Integer.valueOf(0));
+    }
+    
+    @Benchmark
+    public void getNonCached(Blackhole bh) {
+        bh.consume(Integer.valueOf(49477482));
+    }
+    
+}
