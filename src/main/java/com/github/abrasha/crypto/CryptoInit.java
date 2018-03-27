477d0cb61e10c1262073731fd5e4afcc3960000e
abrasha <aabrasha@gmail.com>
2018-03-27 12:00:10 +0300

    Fix CryptoInit benchmark: should be initialized with different IV every time

--- src/main/java/com/github/abrasha/crypto/CryptoInit.java
+++ src/main/java/com/github/abrasha/crypto/CryptoInit.java
@@ -5,8 +5,10 @@ import org.openjdk.jmh.infra.Blackhole;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.security.SecureRandom;
 import java.util.concurrent.TimeUnit;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -24,6 +26,7 @@ public class CryptoInit {
     
     private static class KeyContainer {
         static final Key key;
+        static final SecureRandom SECURE_RANDOM = new SecureRandom();
         
         static {
             try {
@@ -37,31 +40,29 @@ public class CryptoInit {
             }
         }
         
+        static IvParameterSpec generateIV() {
+            byte[] bytes = new byte[16];
+            SECURE_RANDOM.nextBytes(bytes);
+            return new IvParameterSpec(bytes);
+        }
+        
     }
     
     @State(Scope.Benchmark)
     public static class CipherContainer {
         
-        private ThreadLocal<Cipher> cipherThreadLocal = new ThreadLocal<>();
-        
-        private static Cipher initCipher(){
+        private ThreadLocal<Cipher> cipherThreadLocal = ThreadLocal.withInitial(() -> {
             try {
-    
-                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-                cipher.init(Cipher.ENCRYPT_MODE, KeyContainer.key);
-                return cipher;
-            } catch (Exception e){
+                return Cipher.getInstance("AES/CBC/PKCS5Padding");
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        }
+        });
         
-        public Cipher getCipher(){
-            Cipher cached = cipherThreadLocal.get();
-            if (cached == null){
-                cached = initCipher();
-                cipherThreadLocal.set(cached);
-            }
-            return cached;
+        Cipher getCipher() throws Exception {
+            Cipher cipher = cipherThreadLocal.get();
+            cipher.init(Cipher.ENCRYPT_MODE, KeyContainer.key, KeyContainer.generateIV());
+            return cipher;
         }
     }
     
@@ -71,9 +72,9 @@ public class CryptoInit {
     }
     
     @Benchmark
-    public void withNewCipher(CipherContainer container, Blackhole bh) throws Exception {
+    public void withNewCipher(Blackhole bh) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, KeyContainer.key);
+        cipher.init(Cipher.ENCRYPT_MODE, KeyContainer.key, KeyContainer.generateIV());
         bh.consume(cipher.doFinal(MESSAGE_TO_ENCRYPT));
     }
     
